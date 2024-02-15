be690c1c0541019d58f20fada25267152308e9e3
TheVincentPrice <156710769+TheVincentPrice@users.noreply.github.com>
2024-02-15 21:57:49 +0100

    Update vite.config.ts

--- emma-web-app/vite.config.ts
+++ emma-web-app/vite.config.ts
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
+})
+
+// vite.config.js
+export default defineConfig({
   plugins: [react()],
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/src/main.tsx',
+    },
+  },
 })
